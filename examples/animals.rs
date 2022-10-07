246594c6fbdb18139a78f8e1e99e0fb0fd11e0e9
Harry Liu <hoyunliu@gmail.com>
2022-10-07 23:30:12 +0800

    trim unused code

--- examples/animals.rs
+++ examples/animals.rs
@@ -3,44 +3,24 @@ use box_self::box_self;
 trait Animal {
     fn consume(self);
     fn consume_boxed(self:Box<Self>);
-    fn extra(&self);
 } 
 
 struct Dog{}
 impl Animal for Dog{
     #[box_self(_boxed)]
-    fn consume(mut self) {
+    fn consume(self) {
         println!("Bark");
-    }
-    fn extra(&self){
-
     }
 } 
 
 struct Cat{}
 impl Animal for Cat{
     #[box_self(_boxed)]
-    fn consume(mut self) {
+    fn consume(self) {
         println!("Jump");
     }
-    fn extra(&self){
-        
-    }
 } 
 
-impl Animal for Box<dyn Animal>{
-    fn consume(self) {
-        Animal::consume_boxed(self);
-    }
-
-    fn consume_boxed(self:Box<Self>) {
-        Animal::consume_boxed(*self);
-    }
-
-    fn extra(&self){
-       (**self).extra(); 
-    }
-} 
 
 fn main(){
     let mut animals:Vec<Box<dyn Animal>>=Vec::new();
